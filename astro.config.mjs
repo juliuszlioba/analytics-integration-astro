4a86e8afa6bdc9c60ae6677931b4664ef61af961
Julius Zlioba <julius.zlioba@connect724.at>
2024-02-22 11:27:18 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config'
 import solid from '@astrojs/solid-js'
+//import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solid(), tailwind()]
+	integrations: [
+		solid(),
+		// partytown({
+		// 	config: {
+		// 		debug: true,
+		// 		forward: ['dataLayer.push']
+		// 	}
+		// }),
+		tailwind()
+	]
 })
